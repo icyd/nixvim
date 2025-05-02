0ff88b64f68c0c56055f4eb05f21542216cbb133
Alberto Vázquez <beto.v25@gmail.com>
2025-05-02 20:41:14 +0200

    feat: add command to toggle format on save

--- modules/lsp/format.nix
+++ modules/lsp/format.nix
@@ -29,6 +29,14 @@
       };
       settings = {
         default_format_opts.lsp_format = "fallback";
+        format_on_save.__raw = ''
+          function(bufnr)
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+              return
+            end
+              return { timeout_ms = 500, lsp_format = "fallback" }
+          end
+        '';
         formatters_by_ft = rec {
           c = ["clang-format"];
           cpp = c;
@@ -50,6 +58,47 @@
           squeeze_blanks.command = lib.getExe' pkgs.coreutils "cat";
         };
       };
+    };
+  };
+  userCommands = {
+    FormatDisable = {
+      bang = true;
+      command.__raw = ''
+        function(args)
+          if args.bang then
+            vim.b.disable_autoformat = true
+          else
+            vim.g.disable_autoformat = true
+          end
+        end
+      '';
+      desc = "Disable auto format on save";
+    };
+    FormatEnable = {
+      bang = true;
+      command.__raw = ''
+        function(args)
+          if args.bang then
+            vim.b.disable_autoformat = false
+          else
+            vim.g.disable_autoformat = false
+          end
+        end
+      '';
+      desc = "Enable auto format on save";
+    };
+    FormatToggle = {
+      bang = true;
+      command.__raw = ''
+        function(args)
+          if args.bang then
+            vim.b.disable_autoformat = not vim.b.disable_autoformat
+          else
+            vim.g.disable_autoformat = not vim.g.disable_autoformat
+          end
+        end
+      '';
+      desc = "Toggle auto format on save";
     };
   };
 }
