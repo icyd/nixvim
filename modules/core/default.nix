31482947e51a66225dfb5f340e0b6dfc1138fba4
Alberto Vázquez <avazquez@contractor.ea.com>
2025-10-21 22:59:50 +0200

    feat: add rules to nvim-autopairs

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -150,6 +150,65 @@
       nvim-autopairs = {
         enable = true;
         # lazyLoad.settings.event = "BufReadPre";
+        luaConfig.post = ''
+          local autopairs = require("nvim-autopairs")
+          local rule = require("nvim-autopairs.rule")
+          local conds = require("nvim-autopairs.conds")
+          local utils = require("nvim-autopairs.utils")
+          local ts_conds = require("nvim-autopairs.ts-conds")
+
+          local is_not_ts_node_comment_one_back = function()
+            return function(info)
+              local p = vim.api.nvim_win_get_cursor(0)
+              local pos_adjusted = {p[1] - 1, p[2] - 1}
+              vim.treesitter.get_parser():parse()
+              local target = vim.treesitter.get_node({pos = pos_adjusted, ignore_injections = false})
+              if target ~= nil and utils.is_in_table({"comment"}, target:type()) then
+                return false
+              end
+
+              local rest_of_line = info.line:sub(info.col)
+              return rest_of_line:match('^%s*$') ~= nil
+            end
+          end
+          local brackets = {{ '(', ')' }, { '[', ']' }, { '{', '}' }}
+
+          autopairs.add_rules({
+            rule("<", ">"):with_pair(conds.before_regex('%a+:?:?$', 3)):with_move(function(opts)
+              return opts.char == '>'
+            end),
+            rule("= ", ";", "nix"):with_pair(is_not_ts_node_comment_one_back()):set_end_pair_length(1),
+            rule("{", "},", "lua"):with_pair(ts_conds.is_ts_node({"table_constructor"})),
+            rule("'", "',", "lua"):with_pair(ts_conds.is_ts_node({"table_constructor"})),
+            rule('"', '",', "lua"):with_pair(ts_conds.is_ts_node({"table_constructor"})),
+            rule(' ', ' '):with_pair(function(opts)
+              local pair = opts.line:sub(opts.col - 1, opts.col)
+              return vim.tbl_contains({
+                brackets[1][1] .. brackets[1][2],
+                brackets[2][1] .. brackets[2][2],
+                brackets[3][1] .. brackets[3][2],
+              }, pair)
+            end):with_move(conds.none()):with_cr(conds.none()):with_del(function(opts)
+              local col = vim.api.nvim_win_get_cursor(0)[2]
+              local context = opts.line:sub(col - 1, col + 2)
+              return vim.tbl_contains({
+                brackets[1][1] .. ' ' .. brackets[1][2],
+                brackets[2][1] .. ' ' .. brackets[2][2],
+                brackets[3][1] .. ' ' .. brackets[3][2],
+              }, context)
+            end),
+          })
+
+          for _, bracket in pairs(brackets) do
+            autopairs.add_rules({
+              rule(bracket[1] .. ' ', ' ' .. bracket[2]):with_pair(conds.none()):with_move(function(opts)
+                return opts.char == bracket[2]
+              end):with_del(conds.none()):use_key(bracket[2]):replace_map_cr(function(_)
+                return '<C-c>2xi<CR><C-c>O'
+              end),
+            })
+          end
+        '';
         settings = {
           check_ts = true;
           disable_in_macro = true;
