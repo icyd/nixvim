9f57ed105c933fd8a47c3429549a9483443bb5c5
Alberto Vázquez <aj.vazquez@globant.com>
2025-12-12 07:38:06 +0100

    feat: add snacks.picker keybindings

--- modules/additional-plugins.nix
+++ modules/additional-plugins.nix
@@ -25,124 +25,191 @@
       cfg = config.plugins.snacks;
     in
       builtins.map mkKeyMap (lib.optionals (cfg.enable && cfg.settings.bufdelete.enabled) [
-        {
-          action = mkRaw ''
-            function()
-              require("snacks").bufdelete.delete()
-            end
-          '';
-          key = "<localleader>q";
-          options.desc = "BufDelete";
-        }
-        {
-          action = mkRaw ''
-            function()
-              require("snacks").bufdelete.delete({force=true})
-            end
-          '';
-          key = "<localleader>Q";
-          options.desc = "BufDelete force";
-        }
-        {
-          action = mkRaw ''
-            function()
-              require("snacks").bufdelete.all()
-            end
-          '';
-          key = "<localleader>bA";
-          options.desc = "BufDelete all";
-        }
-        {
-          action = mkRaw ''
-            function()
-              require("snacks").bufdelete.other()
-            end
-          '';
-          key = "<localleader>bO";
-          options.desc = "BufDelete other";
-        }
-        {
-          action = mkRaw ''
-            function()
-              require("snacks").picker.buffers()
-            end
-          '';
-          key = "<leader>b";
-          options.desc = "Buffers";
-        }
-        {
-          action = mkRaw ''
-            function()
-              require("snacks").picker.files()
-            end
-          '';
-          key = "<leader>ff";
-          options.desc = "Find git files";
-        }
-        {
-          action = mkRaw ''
-            function()
-              require("snacks").picker.files()
-            end
-          '';
-          key = "<leader>fF";
-          options.desc = "Find files";
-        }
-        {
-          action = mkRaw ''
-            function()
-              require("snacks").picker.files({ cwd = os.getenv("DOTFILES") })
-            end
-          '';
-          key = "<leader>fv";
-          options.desc = "Find files in DOTFILES";
-        }
-        {
-          action = mkRaw ''
-            function()
-              require("snacks").picker.grep()
-            end
-          '';
-          key = "<leader>fs";
-          options.desc = "Grep";
-        }
-        {
-          action = mkRaw ''
-            function()
-              require("snacks").picker.projects()
-            end
-          '';
-          key = "<leader>fp";
-          options.desc = "Projects";
-        }
-        {
-          action = mkRaw ''
-            function()
-              require("snacks").picker.recent()
-            end
-          '';
-          key = "<leader>fr";
-          options.desc = "Recent";
-        }
-        {
-          action = mkRaw ''
-            function()
-              require("snacks").picker.explorer()
-            end
-          '';
-          key = "<leader>fb";
-          options.desc = "File explorer";
-        }
-        {
-          action = mkRaw ''
-            function()
-              require("snacks").picker.git_branches()
-            end
-          '';
-          key = "<leader>gb";
-          options.desc = "Git branches";
-        }
-      ]);
+          {
+            action = mkRaw ''
+              function()
+                require("snacks").bufdelete.delete()
+              end
+            '';
+            key = "<localleader>q";
+            options.desc = "BufDelete";
+          }
+          {
+            action = mkRaw ''
+              function()
+                require("snacks").bufdelete.delete({force=true})
+              end
+            '';
+            key = "<localleader>Q";
+            options.desc = "BufDelete force";
+          }
+          {
+            action = mkRaw ''
+              function()
+                require("snacks").bufdelete.all()
+              end
+            '';
+            key = "<localleader>bA";
+            options.desc = "BufDelete all";
+          }
+          {
+            action = mkRaw ''
+              function()
+                require("snacks").bufdelete.other()
+              end
+            '';
+            key = "<localleader>bO";
+            options.desc = "BufDelete other";
+          }
+          {
+            action = mkRaw ''
+              function()
+                require("snacks").picker.buffers({current = false, sort_lastused = true})
+              end
+            '';
+            key = "<leader>b";
+            options.desc = "Buffers";
+          }
+          {
+            action = mkRaw ''
+              function()
+                require("snacks").picker.files()
+              end
+            '';
+            key = "<leader>ff";
+            options.desc = "Find git files";
+          }
+          {
+            action = mkRaw ''
+              function()
+                require("snacks").picker.files()
+              end
+            '';
+            key = "<leader>fF";
+            options.desc = "Find files";
+          }
+          {
+            action = mkRaw ''
+              function()
+                require("snacks").picker.files({ cwd = os.getenv("DOTFILES") })
+              end
+            '';
+            key = "<leader>fv";
+            options.desc = "Find files in DOTFILES";
+          }
+          {
+            action = mkRaw ''
+              function()
+                require("snacks").picker.grep()
+              end
+            '';
+            key = "<leader>fs";
+            options.desc = "Grep";
+          }
+          {
+            action = mkRaw ''
+              function()
+                require("snacks").picker.projects()
+              end
+            '';
+            key = "<leader>fp";
+            options.desc = "Projects";
+          }
+          {
+            action = mkRaw ''
+              function()
+                require("snacks").picker.recent()
+              end
+            '';
+            key = "<leader>fr";
+            options.desc = "Recent";
+          }
+          {
+            action = mkRaw ''
+              function()
+                require("snacks").picker.explorer({auto_close = true, follow_file = false})
+              end
+            '';
+            key = "<leader>fb";
+            options.desc = "File explorer";
+          }
+          {
+            action = mkRaw ''
+              function()
+                require("snacks").picker.explorer({auto_close = true, follow_file = true})
+              end
+            '';
+            key = "<leader>fB";
+            options.desc = "File explorer relative to current file";
+          }
+          {
+            action = mkRaw ''
+              function()
+                require("snacks").picker.git_branches()
+              end
+            '';
+            key = "<leader>gb";
+            options.desc = "Git branches";
+          }
+          {
+            action = mkRaw ''
+              function()
+                require("snacks").picker.command_history()
+              end
+            '';
+            key = "<leader>f;";
+            options.desc = "Command history";
+          }
+          {
+            action = mkRaw ''
+              function()
+                require("snacks").picker.search_history()
+              end
+            '';
+            key = "<leader>f/";
+            options.desc = "Search history";
+          }
+          {
+            action = mkRaw ''
+              function()
+                require("snacks").picker.qflist()
+              end
+            '';
+            key = "<leader>fq";
+            options.desc = "Quickfix list";
+          }
+          {
+            action = mkRaw ''
+              function()
+                require("snacks").picker.undo()
+              end
+            '';
+            key = "<leader>fU";
+            options.desc = "Undo";
+          }
+        ]
+        ++ (lib.optionals config.plugins.todo-comments.enable [
+          {
+            action = mkRaw ''
+              function()
+                require("snacks").picker.todo_comments()
+              end
+            '';
+            key = "<leader>fT";
+            options.desc = "Todo comments";
+          }
+        ])
+        ++ (lib.optionals config.plugins.trouble.enable [
+          {
+            action = mkRaw ''
+              function()
+                require("snacks").picker.todo_comments()
+              end
+            '';
+            key = "<leader>ft";
+            options.desc = "Todo comments";
+          }
+        ]));
     keymaps =
       builtins.map mkKeyMap [
         {
@@ -228,7 +295,7 @@
           bufdelete.enabled = true;
           debug.enabled = true;
           indent.enabled = true;
-          picker.enablad = true;
+          picker.enabled = true;
           scope.enabled = true;
           scroll.enabled = true;
           statuscolumn.enabled = true;
